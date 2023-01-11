99c591eee12e7ee1b45471c42a294b0fe7c7ee06
Salmanul Fariz N <salmanulfariz997@gmail.com>
2023-01-11 16:23:52 +0530

    clean up the api section

--- src/api/authRouter.js
+++ src/api/authRouter.js
@@ -8,73 +8,15 @@ const router = express.Router();
 const service = new AuthenticationService();
 
 // View page of Signup
-router.get('/signup', (req, res) => {
-  try {
-    res.status(200).json({
-      status: 'Success',
-      message: 'View Signup Page',
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.get('/signup', service.UserSignupGet);
 
 // Register user Signup
-router.post('/signup', async (req, res) => {
-  try {
-    const { userName, email, password } = req.body;
-    let data;
-
-    if (req.query.userNameExist) {
-      // Check username exist
-      data = await service.UsernameExist(req.query.userNameExist);
-    } else {
-      // Create a new user
-      data = await service.UserSignup(
-        { userName, email, password },
-        req.headers.host
-      );
-    }
-
-    return res.status(data.statusCode).json({
-      status: data.status,
-      data: data.result,
-    });
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
-});
+router.post('/signup', service.UserSignupPost);
 
 // Verification Email
-router.get('/verify', async (req, res) => {
-  try {
-    const token = req.query.token;
-    const data = await service.EmailValidation(token);
-
-    return res.status(data.statusCode).json({
-      status: data.status,
-      data: data.result,
-    });
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
-});
+router.get('/verify', service.EmailValidationGet);
 
 // Check Email Verification completed
-router.post('/checkVerify', async (req, res) => {
-  try {
-    const data = await service.CheckVerifyMail(req.body.jwt);
-
-    return res.status(data.statusCode).json({
-      status: data.status,
-      data: data.result,
-    });
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
-});
+router.post('/checkVerify', service.CheckVerifyMailPost);
 
 module.exports = router;
--- src/services/authService.js
+++ src/services/authService.js
@@ -5,86 +5,113 @@ const AuthenticationRepository = require('../database/repository/authRepository'
 const { nullValidation } = require('../utils/dataValidation');
 const { resDataFormat } = require('../utils/formatData');
 
+const repository = new AuthenticationRepository();
+
 // All Business logic will be here
 class AuthenticationService {
-  constructor() {
-    this.repository = new AuthenticationRepository();
-  }
-
-  // Register a user
-  async UserSignup(userData, host) {
+  // View user signup page
+  async UserSignupGet(req, res) {
     try {
-      const { userName, email, password } = userData;
-
-      // Check the values are null
-      const nullCheck = nullValidation(userName, email, password);
-      if (nullCheck) {
-        return resDataFormat(200, 'Field-Error', 'Please fill the fields');
-      }
-
-      // Email Validation
-      const ckeckEmail = await emailValidator.validate(email);
-      if (!ckeckEmail.valid) {
-        return resDataFormat(200, 'Email-Error', 'Please enter valid Mail');
-      }
-
-      // Password Validation
-      if (password.length < 6) {
-        return resDataFormat(
-          200,
-          'Password-Error',
-          'Please enter Strong Password'
-        );
-      }
-
-      // User name Length checking
-      if (userName.length < 4) {
-        return resDataFormat(
-          200,
-          'UserName-Error',
-          'Please enter strong user name'
-        );
-      }
-
-      const data = await this.repository.CreateUser(userData, host);
-
-      return data;
+      res.status(200).json({
+        status: 'Success',
+        message: 'View Signup Page',
+      });
     } catch (err) {
       console.log(err);
     }
   }
 
-  // Check user name is exist
-  async UsernameExist(userName) {
+  // Register a user
+  async UserSignupPost(req, res) {
     try {
-      const data = await this.repository.UserNameExist(userName);
+      const { userName, email, password } = req.body;
+      let data;
 
-      return resDataFormat(200, data ? 'Fail' : 'Success', data);
+      if (req.query.userNameExist) {
+        // Check username exist
+        const isUsername = await repository.UserNameExist(
+          req.query.userNameExist
+        );
+
+        data = resDataFormat(200, isUsername ? 'Fail' : 'Success', isUsername);
+      } else {
+        // Check the values are null
+        const nullCheck = nullValidation(userName, email, password);
+        if (nullCheck) {
+          return resDataFormat(200, 'Field-Error', 'Please fill the fields');
+        }
+
+        // Email Validation
+        const ckeckEmail = await emailValidator.validate(email);
+        if (!ckeckEmail.valid) {
+          return resDataFormat(200, 'Email-Error', 'Please enter valid Mail');
+        }
+
+        // Password Validation
+        if (password.length < 6) {
+          return resDataFormat(
+            200,
+            'Password-Error',
+            'Please enter Strong Password'
+          );
+        }
+
+        // User name Length checking
+        if (userName.length < 4) {
+          return resDataFormat(
+            200,
+            'UserName-Error',
+            'Please enter strong user name'
+          );
+        }
+
+        data = await repository.CreateUser(
+          { userName, email, password },
+          req.headers.host
+        );
+      }
+
+      res.status(data.statusCode).json({
+        status: data.status,
+        data: data.result,
+      });
     } catch (err) {
       console.log(err);
     }
   }
 
   // Verification of Email
-  async EmailValidation(emailToken) {
+  async EmailValidationGet(req, res) {
     try {
-      const data = await this.repository.EmailVerification(emailToken);
+      const token = req.query.token;
 
-      return resDataFormat(
+      const isVerify = await repository.EmailVerification(token);
+
+      const data = resDataFormat(
         200,
-        data ? 'Success' : 'Failed',
-        data ? 'verifyed' : 'Oops..!'
+        isVerify ? 'Success' : 'Failed',
+        isVerify ? 'verifyed' : 'Oops..!'
       );
+
+      return res.status(data.statusCode).json({
+        status: data.status,
+        data: data.result,
+      });
     } catch (err) {
       console.log(err);
     }
   }
 
-  async CheckVerifyMail(userToken) {
+  async CheckVerifyMailPost(req, res) {
     try {
-      const data = await this.repository.VerifyMail(userToken);
+      const isVerified = await repository.VerifyMail(req.body.jwt);
 
-      return resDataFormat(200, 'Success', data);
+      const data = resDataFormat(200, 'Success', isVerified);
+
+      return res.status(data.statusCode).json({
+        status: data.status,
+        data: data.result,
+      });
     } catch (err) {
       console.log(err);
     }
